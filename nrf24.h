73f89a25012b9d312be449bac2933394b1e77acb
matt-walker0 <78490185+matt-walker0@users.noreply.github.com>
2021-09-26 23:02:29 +0100

    Update nrf24.h

--- nrf24.h
+++ nrf24.h
@@ -4,11 +4,13 @@ extern "C"
 #endif
 
 bool nrf24Init(uint8_t address[2][6], uint8_t spi_bus, uint8_t sck_pin, uint8_t tx_pin, uint8_t rx_pin, uint8_t ce_pin, uint8_t csn_pin);
+void nrf24SetupIRQ(uint8_t irq_pin, void (*irq_handler) (uint gpio, uint32_t event));
 bool nrf24HasNewData();
 bool nrf24SendData(uint8_t buffer[5]);
 void nrf24ReadData(uint8_t buffer[5]);
-bool nrf24InterruptHandle();
 void nrf24TestRadio();
+void nrf24StartListening();
+void nrf24StopListening();
 
 #ifdef __cplusplus
 }
