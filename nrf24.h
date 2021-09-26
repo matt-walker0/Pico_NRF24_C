5b73c954f1936caf703ee3a399cfea8bc5efdbea
matt-walker0 <78490185+matt-walker0@users.noreply.github.com>
2021-09-26 12:35:37 +0100

    Update nrf24.h

--- nrf24.h
+++ nrf24.h
@@ -3,7 +3,7 @@ extern "C"
 {
 #endif
 
-bool nrf24Init(uint8_t address[][6], uint8_t SPI_BUS, uint8_t SCK_PIN, uint8_t TX_PIN, uint8_t RX_PIN, uint8_t CE_PIN, uint8_t CSN_PIN);
+bool nrf24Init(uint8_t address[2][6], uint8_t spi_bus, uint8_t sck_pin, uint8_t tx_pin, uint8_t rx_pin, uint8_t ce_pin, uint8_t csn_pin);
 bool nrf24HasNewData();
 bool nrf24SendData(uint8_t buffer[5]);
 void nrf24ReadData(uint8_t buffer[5]);
