81c793995084f1be0a2e402812f38c3dbf9e570f
matt-walker0 <78490185+matt-walker0@users.noreply.github.com>
2021-09-26 10:46:59 +0100

    Update nrf24.h
    fixed header file

--- nrf24.h
+++ nrf24.h
@@ -5,7 +5,7 @@ extern "C"
 
 bool nrf24Init(uint8_t address[][6], uint8_t SPI_BUS, uint8_t SCK_PIN, uint8_t TX_PIN, uint8_t RX_PIN, uint8_t CE_PIN, uint8_t CSN_PIN);
 bool nrf24SendData(uint8_t buffer[5]);
-void nrf24ReadData(uint8_t buffer[5]);
+bool nrf24ReadData(uint8_t buffer[5]);
 bool nrf24InterruptHandle();
 void nrf24TestRadio();
 
