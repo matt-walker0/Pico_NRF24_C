0d9aa56c30e81024912e53f3a973f7975988c9f8
matt-walker0 <78490185+matt-walker0@users.noreply.github.com>
2021-12-11 20:03:13 +0000

    Consolidate libraries

--- nrf24.h
+++ nrf24.h
@@ -9,10 +9,10 @@ extern "C"
 bool nrf24Init(uint8_t address[2][6], uint8_t spi_bus, uint8_t sck_pin, uint8_t tx_pin, uint8_t rx_pin, uint8_t ce_pin, uint8_t csn_pin);
 void nrf24NumberRetries(uint8_t count, uint8_t time_interval);
 void nrf24SetupIRQ(uint8_t irq_pin, void (*irq_handler) (uint gpio, uint32_t event));
-bool nrf24NewDataIRQ();
+bool nrf24RxIRQ();
 bool nrf24HasNewData();
 bool nrf24SendData(uint8_t buffer[], uint8_t len);
-void nrf24ReadData(uint8_t buffer[5]);
+void nrf24ReadData(uint8_t buffer[], uint8_t len);
 void nrf24TestRadio();
 void nrf24StartListening();
 void nrf24StopListening();
