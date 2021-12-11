c36ff340282d73e3151446ff7834920e2d48b09f
matt-walker0 <78490185+matt-walker0@users.noreply.github.com>
2021-12-11 20:12:09 +0000

    Reflect changes in header

--- nrf24.h
+++ nrf24.h
@@ -7,6 +7,7 @@ extern "C"
 #endif
 
 bool nrf24Init(uint8_t address[2][6], uint8_t spi_bus, uint8_t sck_pin, uint8_t tx_pin, uint8_t rx_pin, uint8_t ce_pin, uint8_t csn_pin);
+void nrf24PayloadLength(uint8_t len);
 void nrf24NumberRetries(uint8_t count, uint8_t time_interval);
 void nrf24SetupIRQ(uint8_t irq_pin, void (*irq_handler) (uint gpio, uint32_t event));
 bool nrf24RxIRQ();
