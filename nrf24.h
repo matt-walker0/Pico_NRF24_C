025a511e737a3284ec1276e1c189d4ccc58e455a
matt-walker0 <78490185+matt-walker0@users.noreply.github.com>
2022-01-08 17:31:48 +0000

    Serpate autoack into option

--- nrf24.h
+++ nrf24.h
@@ -8,6 +8,7 @@ extern "C"
 
 bool nrf24Init(uint8_t address[2][6], uint8_t spi_bus, uint8_t sck_pin, uint8_t tx_pin, uint8_t rx_pin, uint8_t ce_pin, uint8_t csn_pin);
 void nrf24PayloadLength(uint8_t len);
+void nrf24AckEnabled(bool enabled);
 void nrf24NumberRetries(uint8_t count, uint8_t time_interval);
 void nrf24SetupIRQ(uint8_t irq_pin, void (*irq_handler) (uint gpio, uint32_t event));
 bool nrf24RxIRQ();
