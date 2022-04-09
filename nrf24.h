bfa04445135264f69212fae32b453d61eaa3c239
matt-walker0 <78490185+matt-walker0@users.noreply.github.com>
2022-04-09 15:32:45 +0100

    Improved function names

--- nrf24.h
+++ nrf24.h
@@ -6,20 +6,20 @@ extern "C"
 {
 #endif
 
-bool nrf24Init(uint8_t address[2][6], uint8_t spi_bus, uint8_t sck_pin, uint8_t tx_pin, uint8_t rx_pin, uint8_t ce_pin, uint8_t csn_pin);
-void nrf24PayloadLength(uint8_t len);
-void nrf24AckEnabled(bool enabled);
-void nrf24NumberRetries(uint8_t count, uint8_t time_interval);
-void nrf24SetupIRQ(uint8_t irq_pin, void (*irq_handler) (uint gpio, uint32_t event));
-bool nrf24RxIRQ();
-bool nrf24HasNewData();
-bool nrf24SendData(uint8_t buffer[], uint8_t len);
-void nrf24ReadData(uint8_t buffer[], uint8_t len);
-void nrf24TestRadio();
-void nrf24StartListening();
-void nrf24StopListening();
-void nrf24LowPWR();
-void nrf24MaxPWR();
+bool NRF24_Init(uint8_t address[2][6], uint8_t spi_bus, uint8_t sck_pin, uint8_t tx_pin, uint8_t rx_pin, uint8_t ce_pin, uint8_t csn_pin);
+void NRF24_PayloadLength(uint8_t len);
+void NRF24_AckEnabled(bool enabled);
+void NRF24_NumberRetries(uint8_t count, uint8_t time_interval);
+void NRF24_SetupIRQ(uint8_t irq_pin, void (*irq_handler) (uint gpio, uint32_t event));
+bool NRF24_RxIRQ();
+bool NRF24_HasNewData();
+bool NRF24_SendData(uint8_t buffer[], uint8_t len);
+void NRF24_ReadData(uint8_t buffer[], uint8_t len);
+void NRF24_TestRadio();
+void NRF24_StartListening();
+void NRF24_StopListening();
+void NRF24_LowPWR();
+void NRF24_MaxPWR();
 
 
 #ifdef __cplusplus
